6942b2f30b34444b5b4c71ce7c08de2a6fdda73a
crackedcd <crackedcd@qq.com>
2017-11-22 16:59:25 +0800

    git add templastes example.

--- springboot_demo/src/main/java/com/cc_testj/demo/controller/WordsController.java
+++ springboot_demo/src/main/java/com/cc_testj/demo/controller/WordsController.java
@@ -3,26 +3,43 @@ package com.cc_testj.demo.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.cc_testj.demo.mybatis.model.Words;
 import com.cc_testj.demo.service.WordService;
 
-@RestController
+@Controller
 public class WordsController {
 
 	@Autowired
 	WordService wordService;
 	
 	@RequestMapping("/words/listAll")
+	@ResponseBody
 	public List<Words> listAll(){
 		return wordService.queryAllWords();
 	}
 	
 	@RequestMapping("/words/query/{word}")
+	@ResponseBody
 	public List<Words> queryByWord(@PathVariable String word){
 		return wordService.queryByWord(word);
 	}
+	
+	@RequestMapping("/words/showWord/{wordParam}")
+	public ModelAndView showWord(@PathVariable String wordParam) {
+		ModelAndView mv = new ModelAndView("/words/showWord");
+		Words word = wordService.queryByWord(wordParam).get(0);
+		mv.addObject("word", word);
+		return mv;
+	}
+	
+	
+	
+	
+	
 }
---
+++ springboot_demo/src/main/resources/templates/words/showWord.ftl
@@ -0,0 +1,12 @@
+<html>
+  <head>
+  	<meta charset="utf-8" />
+  </head>
+  <body>
+  	<table>
+  		<tr>
+  			<td>单词:</td><td>${word.word}</td>
+  		</tr>
+  	</table>
+  </body>
+</html>
